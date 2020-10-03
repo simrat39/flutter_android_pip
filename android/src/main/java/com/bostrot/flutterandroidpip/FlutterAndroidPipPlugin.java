12a08cf33ac4cf43cde355524eb1db259ceaf0e4
simrat39 <simrats169169@gmail.com>
2020-10-03 11:23:32 -0700

    Update to use v2 android embedding

--- android/src/main/java/com/bostrot/flutterandroidpip/FlutterAndroidPipPlugin.java
+++ android/src/main/java/com/bostrot/flutterandroidpip/FlutterAndroidPipPlugin.java
@@ -1,32 +1,72 @@
 package com.bostrot.flutterandroidpip;
 
+import android.app.Activity;
+import android.app.PictureInPictureParams;
 import android.os.Build;
 
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** FlutterAndroidPipPlugin */
-public class FlutterAndroidPipPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  static Registrar _registrar;
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_android_pip");
-    channel.setMethodCallHandler(new FlutterAndroidPipPlugin());
-    _registrar = registrar;
-  }
+/**
+ * FlutterAndroidPipPlugin
+ */
+public class FlutterAndroidPipPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+    private MethodChannel channel;
+    private Activity activity;
 
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("enterPictureInPictureMode")) {
-        if (Build.VERSION.SDK_INT > 24)
-          _registrar.activity().enterPictureInPictureMode();
-        result.success("Android " + android.os.Build.VERSION.RELEASE);
-      } else {
-        result.notImplemented();
-      }
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("enterPictureInPictureMode")) {
+            if (Build.VERSION.SDK_INT > 26) {
+                activity.enterPictureInPictureMode(
+                        new PictureInPictureParams.Builder().build()
+                );
+            } else if (Build.VERSION.SDK_INT > 24) {
+                activity.enterPictureInPictureMode();
+            }
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
     }
-  }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), "flutter_android_pip");
+        channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        channel = null;
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        this.onDetachedFromActivity();
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        this.onAttachedToActivity(binding);
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
+    }
+}
 
